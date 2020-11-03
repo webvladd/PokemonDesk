b1fe46068b3273746f1c9aaeec126774a028096e
Vlad <webvladd@gmail.com>
2020-11-03 20:11:11 +0200

    homework-3

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import cn from 'classnames';
-
-import s from './App.module.scss';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
-	return (
-		<div className={cn(s.header)}>
-			Test This is App Componet!
-		</div>
-	)
-}
-
+  return(
+    <>
+      <Header />
+      <div></div>
+      <Footer />
+    </>
+	);
+};
 export default App;
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './Footer.module.scss';
+
+const Header = () => {
+	return (
+		<footer className={s.footer}>
+			<span>Make with ❤</span>
+			<span>Ours Team</span>
+		</footer>
+	);
+};
+
+export default Header;
