634fafb8b4afcb509799cceb38400d5769d741fc
Vlad <webvladd@gmail.com>
2020-11-08 16:46:39 +0200

    progress

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,18 @@ module.exports = {
         test: /\.svg$/,
         use: ['@svgr/webpack', 'url-loader'],
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
