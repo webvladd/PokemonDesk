39e91b98e536ffc254d81b708d367f9f921fa844
Vlad <webvladd@gmail.com>
2020-11-08 16:23:28 +0200

    progress

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Button from '../../components/Button';
 
 import s from './Home.module.scss';
 
@@ -7,6 +8,9 @@ const HomePage = () => {
   return (
     <div className={s.root}>
       <Header />
+      <div>
+        <Button onClick={() => {}}>See pokemons</Button>
+      </div>
     </div>
   );
 };
