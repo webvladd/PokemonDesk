1b52eb2fe0dcf0a5f68f413a5bfff746232a8831
Vlad <webvladd@gmail.com>
2020-11-18 19:37:09 +0200

    progress

--- src/utils/getUrlWithParamsConfig.ts
+++ src/utils/getUrlWithParamsConfig.ts
@@ -3,7 +3,7 @@ import config from '../config';
 function getUrlWithParamsConfig(endpointConfig: string) {
   const url = {
     ...config.client.server,
-    ...config.client.endpoint[endpointConfig].uri,
+    ...config.client.endpoint[endpointConfig as keyof typeof config.client.endpoint].uri,
   };
 
   return url;
