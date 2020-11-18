99130088485f9eff40f794d93d72e4cc3b15b8fa
Vlad <webvladd@gmail.com>
2020-11-18 19:52:25 +0200

    progress

---
+++ src/hook/getData.ts
@@ -0,0 +1,74 @@
+import { useEffect, useState } from 'react';
+import req from '../utils/request';
+
+// interface PokedexPageProps {
+// 	title?: string;
+// }
+
+interface IStats {
+  attack: number;
+  defense: number;
+  hp: number;
+  specialAttack: number;
+  specialDefense: number;
+  speed: number;
+}
+
+interface IPokemon {
+  abilities: string[];
+  baseExperience: number;
+  height: number;
+  id: number;
+  img: string;
+  isDefault: boolean;
+  name: string;
+  nameClean: string;
+  order: number;
+  stats: IStats;
+  types: string[];
+  weight: number;
+}
+
+interface IPokemonsData {
+  // count: number;
+  // limit: number;
+  // offset: number;
+  pokemons: IPokemon[];
+  total: number;
+}
+
+interface IUsePokemons {
+  data: IPokemonsData;
+  isLoading: boolean;
+  isError: boolean;
+  null: null;
+}
+
+const useData = (endpoint: string): IUsePokemons => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      try {
+        const result = await req(endpoint);
+        setData(result);
+      } catch (e) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getData();
+  }, []);
+
+  return {
+    data,
+    isLoading,
+    isError,
+  };
+};
+
+export default useData;
--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,81 +1,82 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import PokemonCard from '../../components/PokemonCard';
 
 import s from './Pokedex.module.scss';
-import req from '../../utils/request';
+import useData from '../../hook/getData';
+// import req from '../../utils/request';
 
 interface PokedexPageProps {
   title?: string;
 }
+//
+// interface IStats {
+//   attack: number;
+//   defense: number;
+//   hp: number;
+//   specialAttack: number;
+//   specialDefense: number;
+//   speed: number;
+// }
+//
+// interface IPokemon {
+//   abilities: string[];
+//   baseExperience: number;
+//   height: number;
+//   id: number;
+//   img: string;
+//   isDefault: boolean;
+//   name: string;
+//   nameClean: string;
+//   order: number;
+//   stats: IStats;
+//   types: string[];
+//   weight: number;
+// }
+//
+// interface IPokemonsData {
+//   // count: number;
+//   // limit: number;
+//   // offset: number;
+//   pokemons: IPokemon[];
+//   total: number;
+// }
+//
+// interface IUsePokemons {
+//   data: IPokemonsData;
+//   isLoading: boolean;
+//   isError: boolean;
+// }
 
-interface IStats {
-  attack: number;
-  defense: number;
-  hp: number;
-  specialAttack: number;
-  specialDefense: number;
-  speed: number;
-}
-
-interface IPokemon {
-  abilities: string[];
-  baseExperience: number;
-  height: number;
-  id: number;
-  img: string;
-  isDefault: boolean;
-  name: string;
-  nameClean: string;
-  order: number;
-  stats: IStats;
-  types: string[];
-  weight: number;
-}
-
-interface IPokemonsData {
-  // count: number;
-  // limit: number;
-  // offset: number;
-  pokemons: IPokemon[];
-  total: number;
-}
-
-interface IUsePokemons {
-  data: IPokemonsData;
-  isLoading: boolean;
-  isError: boolean;
-}
-
-const usePokemons = (): IUsePokemons => {
-  const [data, setData] = useState({ pokemons: [], total: 0 });
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  useEffect(() => {
-    const getPokemons = async () => {
-      setIsLoading(true);
-      try {
-        const result = await req('getPokemons');
-        setData(result);
-      } catch (e) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    getPokemons();
-  }, []);
-
-  return {
-    data,
-    isLoading,
-    isError,
-  };
-};
+// const usePokemons = (): IUsePokemons => {
+//   const [data, setData] = useState({ pokemons: [], total: 0 });
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [isError, setIsError] = useState(false);
+//
+//   useEffect(() => {
+//     const getPokemons = async () => {
+//       setIsLoading(true);
+//       try {
+//         const result = await req('getPokemons');
+//         setData(result);
+//       } catch (e) {
+//         setIsError(true);
+//       } finally {
+//         setIsLoading(false);
+//       }
+//     };
+//     getPokemons();
+//   }, []);
+//
+//   return {
+//     data,
+//     isLoading,
+//     isError,
+//   };
+// };
 
 const PokedexPage: React.FC<PokedexPageProps> = ({ title }) => {
-  const { data, isLoading, isError } = usePokemons();
+  const { data, isLoading, isError } = useData('getPokemons');
 
   if (isLoading) {
     return <div>Loading...</div>;
